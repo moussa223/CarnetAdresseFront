deff6ec72e15559f1befe2d6dac6178c12774a2c
moussa223 <mm6824963@gmail.com>
2023-01-07 02:03:00 +0100

    initialisation du repos

---
+++ pangocarnet/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {InscriptionComponent} from "./inscription/inscription.component";
+
+const routes: Routes = [{path: '', component: InscriptionComponent },];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
